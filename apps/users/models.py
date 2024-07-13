91d0d6dee319acdeb7c51061658bd8aa15303972
hx9ine <hxn.iitism@gmail.com>
2024-07-13 23:13:47 +0530

    Added CustomUser Model.

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    USER = (
+        (1, 'ADMIN'),
+        (2, 'TEACHER'),
+        (3, 'STUDENT')
+    )
+
+    user_type = models.CharField(choices=USER, max_length=20, default=1)
+    profile_pic = models.ImageField(upload_to='media/profile_pic')
+    city = models.CharField(max_length=200, blank=True)
+    country = models.CharField(max_length=200, blank=True)
